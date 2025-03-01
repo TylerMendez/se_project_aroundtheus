2e1b74b29769c13c63578b2b0cafa99e567d45ff
TylerMendez <skate-skate@live.com>
2025-02-28 22:17:14 -0500

    Revert "se_project_aroundtheus"

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,16 +1,11 @@
 import Popup from "./Popup.js";
-
 export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
     this._form = this._popup.querySelector("form");
-    if (!this._form) {
-      throw new Error(`Form element not found in popup with selector ${popupSelector}`);
-    }
+
     this._inputList = Array.from(this._form.querySelectorAll("input"));
-    this._submitButton = this._form.querySelector("button[type='submit']");
-    this._defaultButtonText = this._submitButton.textContent;
   }
 
   setEventListeners() {
@@ -19,6 +14,8 @@ export default class PopupWithForm extends Popup {
       e.preventDefault();
       const formData = this._getInputValues();
       this._handleFormSubmit(formData);
+      this.close();
+      this._form.reset();
     });
   }
 
@@ -28,18 +25,5 @@ export default class PopupWithForm extends Popup {
       inputValues[input.name] = input.value;
     });
     return inputValues;
-  }
-
-  renderLoading(isLoading) {
-    if (isLoading) {
-      this._submitButton.textContent = "Saving...";
-    } else {
-      this._submitButton.textContent = this._defaultButtonText;
-    }
-  }
-
-  close() {
-    super.close();
-    this._form.reset();
   }
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,26 +1,18 @@
 export default class UserInfo {
-  constructor({ nameSelector, jobSelector, avatarSelector }) {
-    this._name = document.querySelector(nameSelector);
-    this._job = document.querySelector(jobSelector);
-    this._avatar = document.querySelector(avatarSelector);
-
-    if (!this._name || !this._job || !this._avatar) {
-      throw new Error("One or more selectors are invalid");
-    }
+  constructor({ nameSelector, jobSelector }) {
+    this._name = document.querySelector(nameSelector); 
+    this._job = document.querySelector(jobSelector);  
   }
-  
-  setUserInfo({ name, job, avatar }) {
-    this._name.textContent = name;
-    this._job.textContent = job;
-    if (avatar) {
-      this._avatar.src = avatar;
-    }
+  setUserInfo({ name, job }) {
+    if (this._name && this._job) {
+      this._name.textContent = name;  
+      this._job.textContent = job;  
+    } 
   }
-  
   getUserInfo() {
     return {
       name: this._name.textContent,
-      job: this._job.textContent
+      job: this._job.textContent,
     };
   }
 }
